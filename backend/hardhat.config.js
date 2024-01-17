c0ee18f4a7d19eadcd42052c1e5179ea18c31717
Guilherme Seletti <guiseletti@gmail.com>
2024-01-17 19:56:14 +0000

    Backend_hardhat.config_update

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -2,5 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  paths: {
+    artifacts: "../frontend/src/artifacts",
+  },
+  networks:{
+    hardhat: {
+      chainId: 1337
+    }
+  },
   solidity: "0.8.19",
 };
